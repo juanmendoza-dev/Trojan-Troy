79fd1d9c3cc31730f65ee4526c17829fc2c172b4
cookie <juanmendoza6159@gmail.com>
2026-07-19 18:29:39 -0400

    Add design tokens and shared animation keyframes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import "./styles/keyframes.css";
+import "./styles/tokens.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
