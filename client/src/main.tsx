87ac430532591152934d32e1f5c074ed742773a0
cookie <juanmendoza6159@gmail.com>
2026-07-19 21:00:37 -0400

    Self-host the Iris/Pulse theme fonts instead of loading them from Google

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "./theme/ThemeContext";
 import { ThemeSwitcher } from "./theme/ThemeSwitcher";
+import "./styles/fonts.css";
 import "./styles/keyframes.css";
 import "./styles/tokens.css";
 
