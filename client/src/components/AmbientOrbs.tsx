4fd5af8e1fd24472638f4f246600f0a88409d1b5
cookie <juanmendoza6159@gmail.com>
2026-07-20 00:18:52 -0400

    Add shared ambient orbs component

---
+++ client/src/components/AmbientOrbs.tsx
@@ -0,0 +1,10 @@
+import "./AmbientOrbs.css";
+
+export function AmbientOrbs() {
+  return (
+    <>
+      <div className="ambient-orbs__orb ambient-orbs__orb--one" />
+      <div className="ambient-orbs__orb ambient-orbs__orb--two" />
+    </>
+  );
+}
