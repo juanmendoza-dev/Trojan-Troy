5baecd80cb374f34b0cd67d502746f5fad4ddda7
cookie <juanmendoza6159@gmail.com>
2026-07-19 20:22:40 -0400

    Wire theme system and loading screen into the app state machine

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { ThemeProvider } from "./theme/ThemeContext";
+import { ThemeSwitcher } from "./theme/ThemeSwitcher";
 import "./styles/keyframes.css";
 import "./styles/tokens.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+      <ThemeSwitcher />
+    </ThemeProvider>
   </StrictMode>
 );
